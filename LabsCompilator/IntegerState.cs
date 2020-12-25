cc556534169c923d0d670a2f1d25b664d8fa71fb
Атай <51788708+Regingman@users.noreply.github.com>
2020-12-25 22:47:39 +0600

    Initial commit

---
+++ LabsCompilator/IntegerState.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabsCompilator
+{
+    public class Process
+    {
+        public class IntegerState
+        {
+            readonly ProcessState CurrentState;
+            readonly Command Command;
+
+            public IntegerState(ProcessState processState, Command command)
+            {
+                CurrentState = processState;
+                Command = command;
+            }
+            public override int GetHashCode()
+            {
+                return 17 + 31 * CurrentState.GetHashCode() + 31 * Command.GetHashCode();
+            }
+
+            public override bool Equals(object obj)
+            {
+                IntegerState other = obj as IntegerState;
+                return other != null && this.CurrentState == other.CurrentState && this.Command == other.Command;
+            }
+
+
+
+        }
+
+
+        public override string ToString()
+        {
+            switch (CurrentState)
+            {
+                case ProcessState.Active: return "Active";
+                case ProcessState.Inactive: return "Inactive";
+                case ProcessState.FirstPhase: return "FirstPhase";
+                case ProcessState.SecondPhase: return "SecondPhase";
+                case ProcessState.Terminated: return "Terminated";
+                default: return "error";
+            }
+        }
+
+        Dictionary<IntegerState, ProcessState> transitions;
+        public ProcessState CurrentState { get; private set; }
+
+        public Process()
+        {
+            CurrentState = ProcessState.FirstPhase;
+            transitions = new Dictionary<IntegerState, ProcessState>
+            {
+                { new IntegerState(ProcessState.Inactive, Command.Begin), ProcessState.FirstPhase},
+                { new IntegerState(ProcessState.FirstPhase, Command.Resume), ProcessState.SecondPhase},
+                { new IntegerState(ProcessState.FirstPhase, Command.Exit), ProcessState.Terminated},
+                { new IntegerState(ProcessState.FirstPhase, Command.Exception), ProcessState.Error},
+                { new IntegerState(ProcessState.SecondPhase, Command.Resume), ProcessState.SecondPhase},
+                { new IntegerState(ProcessState.SecondPhase, Command.Exit), ProcessState.Terminated},
+                { new IntegerState(ProcessState.SecondPhase, Command.Exception), ProcessState.Error},
+            };
+        }
+
+        public ProcessState GetNext(Command command)
+        {
+            IntegerState transition = new IntegerState(CurrentState, command);
+            ProcessState nextState;
+            if (!transitions.TryGetValue(transition, out nextState))
+                throw new Exception("Invalid transition: " + CurrentState + " -> " + command);
+            return nextState;
+        }
+
+        public ProcessState MoveNext(Command command)
+        {
+            CurrentState = GetNext(command);
+            return CurrentState;
+        }
+
+    }
+
+    public enum ProcessState
+    {
+        Active,
+        Terminated,
+        Inactive,
+        FirstPhase,
+        Error,
+        SecondPhase
+    }
+
+    public enum Command
+    {
+        Begin,
+        Resume,
+        Exit,
+        Exception
+    }
+
+
+}
---
+++ LabsCompilator/Program.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace LabsCompilator
+{
+    class Program
+    {
+        static Regex regexLetter = new Regex(@"^[\p{L}]+$");
+        static Regex regexNumberFirst = new Regex(@"^[\p{N}]+$");
+        static Regex regexNumber = new Regex(@"^[\p{N}]+$");
+        static Regex regexNumberOrLetter = new Regex(@"^[\p{L}\p{N}]+$");
+        public static string Identificator(string context, int index, int max)
+        {
+            if (context.Length == index + 1)
+            {
+                if (context.Length == 0)
+                {
+                    return "Введите что-нибудь";
+                }
+                if (context.Length >= 1)
+                {
+                    MatchCollection matches = regexLetter.Matches(context[0].ToString());
+                    if (matches.Count > 0)
+                    {
+                        return "Выражение является идентификатором";
+                    }
+                    return "Идентификатор начинается с буквы!";
+
+                }
+                return "Выражение является идентификатором";
+            }
+            index++;
+            MatchCollection matchesTwo = regexNumberOrLetter.Matches(context[index].ToString());
+            if (matchesTwo.Count > 0)
+            {
+                return Identificator(context, index, max);
+            }
+            return "Произошла ошибка: index = " + (index + 1) + " symbol = " + context[index];
+        }
+
+        public static string Number(string context, int index, int max)
+        {
+            if (context.Length == index + 1)
+            {
+                if (context.Length == 0)
+                {
+                    return "Введите что-нибудь";
+                }
+                if (context.Length >= 1)
+                {
+                    MatchCollection matches = regexNumber.Matches(context[0].ToString());
+                    if (matches.Count > 0)
+                    {
+                        return "Выражение является целым числом";
+                    }
+                    return "Целое число должно начинаться с цифр от 1 до 9!";
+
+                }
+                return "Выражение является идентификатором";
+            }
+            if (index == 0)
+            {
+                MatchCollection matches = regexNumber.Matches(context[0].ToString());
+                if (matches.Count < 0)
+                {
+                    return "Целое число должно начинаться с цифр от 1 до 9!";
+                }
+            }
+            index++;
+            MatchCollection matchesTwo = regexNumber.Matches(context[index].ToString());
+            if (matchesTwo.Count > 0)
+            {
+                return Number(context, index, max);
+            }
+            else
+            {
+                return "Произошла ошибка: index = " + (index + 1) + " symbol = " + context[index];
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите выражение: ");
+            string val = "";
+            val = Console.ReadLine();
+            //var result = Identificator(val, 0, val.Length);
+            // var result = Number(val, 0, val.Length);
+            Process process = new Process();
+            int index = 0;
+            for (int i = 0; i < val.Length; i++)
+            {
+                if (process.CurrentState != ProcessState.Error)
+                {
+                    MatchCollection matches = regexNumber.Matches(val[i].ToString());
+                    if (matches.Count == 0)
+                    {
+                        process.MoveNext(Command.Exception);
+                        index = i + 1;
+                        continue;
+                    }
+                    if (i == val.Length - 1 && process.CurrentState != ProcessState.Terminated)
+                    {
+                        process.MoveNext(Command.Exit);
+                        continue;
+                    }
+                    process.MoveNext(Command.Resume);
+                }
+            }
+            
+            if (process.CurrentState == ProcessState.Terminated)
+            {
+                Console.WriteLine("Результат: " + process.ToString());
+            }
+            if(index != 0)
+            {
+                Console.WriteLine("Ошибка в индексе под номером " + index);
+            }
+
+        }
+
+        public class Arifmethic
+        {
+            public char symbol { get; set; }
+            public NumberInt firstElem { get; set; }
+            public NumberInt secondElem { get; set; }
+
+        }
+
+        public class NumberInt
+        {
+            public int value { get; set; }
+            public Arifmethic head { get; set; }
+
+        }
+
+        public bool checkInt(string symbol)
+        {
+            MatchCollection matches = regexNumber.Matches(symbol);
+            if (matches.Count > 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool arifmethic(char symbol)
+        {
+            switch (symbol)
+            {
+                case '/': return true;
+                case '*': return true;
+                case '+': return true;
+                case '-': return true;
+                default: return false;
+            }
+        }
+
+        public string ResultArifmethic(string value)
+        {
+            if (value.Length == 0)
+                return null;
+            int startIndex = 0;
+            int endIndex = 0;
+            Elements element = new Elements();
+            element.first = true;
+            for (int i = 0; i <= value.Length; i++)
+            {
+                if (checkInt(value[i].ToString()))
+                {
+                    endIndex++;
+                }
+                else
+                {
+                    if (startIndex != endIndex)
+                    {
+                        if (NumberCheck(value, startIndex, endIndex))
+                        {
+                            if (element.first == true)
+                            {
+                                element.values = value.Substring(startIndex, endIndex - startIndex);
+                                element.character = Character.integer;
+                            }
+                            else
+                            {
+                                Elements temp = new Elements();
+                                temp.values = value.Substring(startIndex, endIndex - startIndex);
+                                temp.character = Character.integer;
+                                temp.beforeElement = element;
+                            }
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public class Elements
+        {
+            public bool first { get; set; } = false;
+            public string values { get; set; }
+            public Character character { get; set; }
+            public Elements afterElement { get; set; }
+            public Elements beforeElement { get; set; }
+
+            /*public Elements NextElement()
+            {
+                if (beforeElement == null)
+                {
+                    return beforeElement
+                }
+            }*/
+        }
+
+        public enum Character
+        {
+            integer,
+            arithmetic,
+            param,
+            typeParam,
+            undefined
+        }
+
+        public class Rule
+        {
+            public Character character { get; set; }
+            public List<Rule> rules { get; set; }
+        }
+
+        public static bool NumberCheck(string context, int index, int max)
+        {
+            if (max == index + 1)
+            {
+                if (max == 0)
+                {
+                    return false;
+                }
+                if (max >= 1)
+                {
+                    MatchCollection matches = regexNumber.Matches(context[0].ToString());
+                    if (matches.Count > 0)
+                    {
+                        return true;
+                    }
+                    return false;
+
+                }
+                return true;
+            }
+            if (index == 0)
+            {
+                MatchCollection matches = regexNumber.Matches(context[0].ToString());
+                if (matches.Count < 0)
+                {
+                    return false;
+                }
+            }
+            index++;
+            MatchCollection matchesTwo = regexNumber.Matches(context[index].ToString());
+            if (matchesTwo.Count > 0)
+            {
+                return NumberCheck(context, index, max);
+            }
+            else
+            {
+                Console.WriteLine("Произошла ошибка: index = " + (index + 1) + " symbol = " + context[index]);
+                return false;
+            }
+        }
+    }
+}
